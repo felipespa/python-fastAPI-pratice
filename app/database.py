5b9fbc281c377cef6c6c390541cc5e6f6d31e09f
felipespa <luisfelipespa25@gmail.com>
2024-10-29 15:22:45 -0300

    Change database port & Fix async session config

--- app/database.py
+++ app/database.py
@@ -2,19 +2,18 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres:root@127.0.0.1:5432/crud"
+DATABASE_URL = "postgresql+asyncpg://postgres:root@localhost:5433/crud"
 
 async_engine = create_async_engine(DATABASE_URL, echo=True)
 
-AsyncSessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=async_engine,
+async_session = sessionmaker(
+    async_engine, 
+    expire_on_commit=False, 
     class_=AsyncSession
 )
 
 Base = declarative_base()
 
-async def get_db() -> AsyncSession:
-    async with AsyncSessionLocal() as session:
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
         yield session
--- app/routers/product.py
+++ app/routers/product.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from ..database import get_db
+from ..database import get_session
 from ..business.product import ProductItem, create_product, delete_product, get_product, get_product_by_id, update_product
 
 router = APIRouter(prefix="/product", tags=["product"])
 
 @router.get("/", response_model=dict, status_code=status.HTTP_200_OK)
-async def list_product(db: AsyncSession = Depends(get_db)):
+async def list_product(db: AsyncSession = Depends(get_session)):
     result = await get_product(db)
 
     if not result["success"]:
@@ -14,28 +14,28 @@ async def list_product(db: AsyncSession = Depends(get_db)):
     return result
 
 @router.get("/{product_id}", response_model=dict, status_code=status.HTTP_200_OK)
-async def list_product_by_id(product_id: int, db: AsyncSession = Depends(get_db)):
+async def list_product_by_id(product_id: int, db: AsyncSession = Depends(get_session)):
     result = await get_product_by_id(product_id, db)
     if not result['success']:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=result['message'])
     return result
 
 @router.post("/", response_model=dict, status_code=status.HTTP_201_CREATED)
-async def add_product(product: ProductItem, db: AsyncSession = Depends(get_db)):
+async def add_product(product: ProductItem, db: AsyncSession = Depends(get_session)):
     result = await create_product(product, db)
     if not result['success']:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=result['message'])
     return result
 
 @router.put("/{product_id}", response_model=dict, status_code=status.HTTP_200_OK)
-async def update_product(product_id: int, product: ProductItem, db: AsyncSession = Depends(get_db)):
+async def update_product(product_id: int, product: ProductItem, db: AsyncSession = Depends(get_session)):
     result = await update_product(product_id, product, db)
     if not result['success']:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=result['message'])
     return result
 
 @router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def remove_product(product_id: int, db: AsyncSession = Depends(get_db)):
+async def remove_product(product_id: int, db: AsyncSession = Depends(get_session)):
     result = await delete_product(product_id, db)
     if not result['success']:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=result['message'])
