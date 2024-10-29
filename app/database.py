f19120a02a027afbffceb7f84fded0c73e43dc61
felipespa <luisfelipespa25@gmail.com>
2024-10-29 10:55:14 -0300

    Change database user

--- app/database.py
+++ app/database.py
@@ -2,19 +2,19 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://root:root@localhost:5432/crud"
+DATABASE_URL = "postgresql+asyncpg://postgres:root@127.0.0.1:5432/crud"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+async_engine = create_async_engine(DATABASE_URL, echo=True)
 
-async_session = sessionmaker(
+AsyncSessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine,
+    bind=async_engine,
     class_=AsyncSession
 )
 
 Base = declarative_base()
 
 async def get_db() -> AsyncSession:
-    async with async_session() as session:
+    async with AsyncSessionLocal() as session:
         yield session
